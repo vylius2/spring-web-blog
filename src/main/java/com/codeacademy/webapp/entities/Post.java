e3a783cca7021487489a3cc88061acd67c08e856
Vilius <polerisviliux@gmail.com>
2021-07-13 00:33:28 +0300

    Post class added and mapped

---
+++ src/main/java/com/codeacademy/webapp/entities/Post.java
@@ -0,0 +1,39 @@
+package com.codeacademy.webapp.entities;
+
+
+import lombok.Data;
+import lombok.extern.apachecommons.CommonsLog;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="post")
+public class Post {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="title")
+    private String title;
+
+    @Column(name="text")
+    private String text;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime date;
+
+    @ManyToOne(cascade= {CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="profile_id")
+    private Profile profile;
+
+    public Post(){
+
+    }
+}
--- src/main/java/com/codeacademy/webapp/entities/Profile.java
+++ src/main/java/com/codeacademy/webapp/entities/Profile.java
@@ -1,2 +1,42 @@
-package com.codeacademy.webapp.entities;public class profile {
+package com.codeacademy.webapp.entities;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="profile")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="username")
+    private String username;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    public Profile(){
+
+    }
 }
+
